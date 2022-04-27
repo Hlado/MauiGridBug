cafa50acc13c6f2b896d265767268800fb73ac80
Ivan Romanov <iwromanov@gmail.com>
2022-04-28 02:51:56 +0300

    Initial Commit

---
+++ MauiIssue/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiIssue/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace MauiIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		ContentView o;
+
+		for (int i = 0; i < 59; ++i)
+        {
+			o = new ContentView() { Background = new SolidColorBrush(Colors.Green), Content = new Label() };
+			//Label o = new Label() { Background = new SolidColorBrush(Colors.Green) };
+
+			Grid.SetRow(o, i / 10);
+			Grid.SetColumn(o, i % 10);
+
+			grid.Add(o);
+        }
+	}
+}
+
